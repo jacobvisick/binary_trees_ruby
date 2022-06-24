ccf14e8aca35aba0939b3f2d857b5b21dad2f96c
Jacob Visick <jacobvisick@gmail.com>
2022-06-24 18:56:40 -0400

    Initial commit

---
+++ bst.rb
@@ -0,0 +1,113 @@
+class Node
+  attr_accessor :data, :left, :right
+
+  def initialize(value)
+    self.data = value
+    self.left = nil
+    self.right = nil
+  end
+
+  def add_child(value)
+    return if value == nil
+    value.instance_of?(Node) ? child = value : child = Node.new(value)
+
+    if self.data == child.data then
+      puts "Discarding duplicate value #{child.data}"
+      return
+    end
+
+   if child.data < self.data then
+    self.left == nil ? self.left = child : self.left.add_child(child)
+   else
+    self.right == nil ? self.right = child : self.right.add_child(child)
+   end
+  end
+
+end
+
+class Tree
+
+  def initialize(data_array)
+    #remove duplicates and sort data
+    sorted_array = merge_sort(data_array.uniq)
+    #create the tree
+    @root = self.build_tree(sorted_array)
+  end
+
+  def build_tree(data_array)
+    return if data_array.empty?
+
+    root = Node.new(data_array[data_array.length/2])
+    root.add_child( build_tree(data_array[0...data_array.length/2]) )
+    root.add_child( build_tree(data_array[data_array.length/2 + 1..-1]) )
+
+    return root
+  end
+
+  def merge_sort(data_array)
+    return data_array if data_array.length < 2
+
+    left = merge_sort(data_array[0...data_array.length/2])
+    right = merge_sort(data_array[data_array.length/2..-1])
+
+    result = []
+    until left.empty? || right.empty?
+      left[0] < right[0] ? result.push(left.shift) : result.push(right.shift)
+    end
+
+    result = result + left + right
+    result
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def level_order(queue = [@root], accumulator = [], &block)
+    #pull next node from queue
+    node = queue.shift
+
+    #add children to back of queue
+    queue.push(node.left) unless node.left == nil
+    queue.push(node.right) unless node.right == nil
+
+    if block_given?
+      #yield current node to passed block
+      block.call(node.data)
+      #continue through the queue until we reach the end
+      queue.empty? ? return : (level_order(queue, accumulator, &block))
+    else
+      #add current node's data to array so we can return it
+      accumulator.push(node.data)
+      #continue through the queue until we reach the end
+      queue.empty? ? accumulator : level_order(queue, accumulator)
+    end
+
+  end
+
+
+end
+
+def run_tests
+  #generate random sample
+  sample_data = []
+  25.times { sample_data.push(rand(20)) }
+  
+  tree = Tree.new(sample_data)
+
+  puts "Input: #{sample_data}"
+  # make sure tree is balanced
+  tree.pretty_print
+  
+  # #level_order with block
+  output = []
+  tree.level_order { |value| output.push(value) }
+  p output
+
+  # #level_order without block
+  p tree.level_order
+end
+
+run_tests
